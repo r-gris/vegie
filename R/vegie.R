eef7f40535d324fe5400b4b618c2062151574b4d
Michael Sumner <mdsumner@gmail.com>
2016-08-05 23:20:23 +1000

    spb

--- R/vegie.R
+++ R/vegie.R
@@ -34,7 +34,7 @@ buildVRT <- function(dsn, layer) {
 #' @examples
 #' @importFrom rgdal readOGR
 #' @importFrom dplyr copy_to src_sqlite
-#' @importFrom spbabel mtable
+# @importFrom spbabel mtable
 buildDB <- function(dsn, layer, dbfile) {
   db <- dplyr::src_sqlite(dbfile, create = TRUE)
   ## build VRT
@@ -45,9 +45,16 @@ buildDB <- function(dsn, layer, dbfile) {
   x <- rgdal::readOGR(sfile, layer, verbose = FALSE)
 
   ## decompose to tables
-  tabs <- spbabel::mtable(x)
+  tabs <- spbabel:::mtable.Spatial(x)
+  indexes <- list(list("object_"),
+                  list(c("object_", "branch_")),
+                  list(c("vertex_", "branch_")),
+                  list("x_", "y_", "vertex_"))
+
+  print(names(tabs))
   for (i in seq_along(tabs)) {
-    dplyr::copy_to(db, tabs[[i]], name = names(tabs)[i])
+    dplyr::copy_to(db, tabs[[i]], name = names(tabs)[i], indexes = indexes[[i]],
+                   temporary = FALSE)
   }
   # ## do the remaining FIDs
   fid <- 1
@@ -58,9 +65,11 @@ buildDB <- function(dsn, layer, dbfile) {
     if (inherits(x, "try-error")) break;
     fid <- fid + 1
     # ## decompose to tables
-    tabs <- spbabel::mtable(x)
+    tabs <- spbabel:::mtable.Spatial(x)
+    print(fid)
     for (i in seq_along(tabs)) {
-      dplyr::db_insert_into( con = db$con, table = names(tabs)[i], values = tabs[[i]])
+
+      dplyr::db_insert_into( con = db$con, table = names(tabs)[i], values = tabs[[i]], indexes = indexes[[i]])
     }
   }
   db
